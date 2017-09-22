ac4dad0218c2eb5f4323720115437b1391eeb05c
thaomt <minhthaobk94@gmail.com>
2017-09-22 09:35:21 +0700

    get connection between ThaoToDo and database

---
+++ src/main/CategoryDAO.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.util.List;
+
+public interface CategoryDAO {
+    List<Category> getAllCategories();
+
+    Category getCategory(int id);
+
+    int addCategory(Category category);
+
+    int updateCategory(Category category);
+
+    int deleteCategory(Category category);
+
+}
